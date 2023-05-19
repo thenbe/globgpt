a04a9d4c6953110a6ceea6abf637cbeb020d0a0a
nbe <aleisa.naser@gmail.com>
2023-05-19 05:43:07 +0300

    [wip] define agent

--- packages/globgpt/src/chat.ts
+++ packages/globgpt/src/chat.ts
@@ -1,13 +1,22 @@
 import { ConversationalRetrievalQAChain } from 'langchain/chains'
+import { Calculator } from 'langchain/tools/calculator'
+import { initializeAgentExecutorWithOptions } from 'langchain/agents'
+import { SerpAPI } from 'langchain/tools'
 import { OpenAI } from 'langchain/llms/openai'
 import type { ChainValues } from 'langchain/dist/schema'
 import { type RetrieveByNameArgs, retrieveByName } from './retrieve'
+import { prompt } from './prompts'
 
 // https://js.langchain.com/docs/modules/chains/index_related_chains/conversational_retrieval
 
 // export async function chat( { name, modelName = 'gpt-3.5-turbo', text, }: ChatArgs,
 export async function chat(args: ChatArgs): Promise<ChainValues> {
-  const { modelName = 'gpt-3.5-turbo', text } = args
+  // return await chatWithLLM(args)
+  return await chatWithAgent(args)
+}
+
+async function chatWithLLM(args: ChatArgs): Promise<ChainValues> {
+  const { modelName = 'gpt-4', text } = args
 
   // Retrieve the vector store
   const vector = await retrieveByName(args)
@@ -29,6 +38,56 @@ export async function chat(args: ChatArgs): Promise<ChainValues> {
   const res = await chain.call({ question: text, chat_history: chatHistory })
 
   return res
+}
+
+async function chatWithAgent(args: ChatArgs): Promise<ChainValues> {
+  process.env.LANGCHAIN_HANDLER = 'langchain'
+  const { modelName = 'gpt-4', text } = args
+
+  // Initialize the LLM to use to answer the question
+  const model = new OpenAI({
+    modelName,
+    verbose: true,
+    temperature: 0,
+  })
+
+  const tools = [
+    new SerpAPI(process.env.SERPAPI_API_KEY, {
+      location: 'Austin,Texas,United States',
+      hl: 'en',
+      gl: 'us',
+    }),
+    new Calculator(),
+  ]
+
+  const executor = await initializeAgentExecutorWithOptions(tools, model, {
+    agentType: 'chat-conversational-react-description',
+    verbose: true,
+    agentArgs: {
+      systemMessage: prompt, // WARN: not ideal
+    },
+  })
+  console.log('Loaded agent.')
+
+  const input0 = 'hi, i am bob'
+
+  const result0 = await executor.call({ input: input0 })
+
+  console.log(`Got output ${result0.output}`)
+
+  const input1 = 'whats my name?'
+
+  const result1 = await executor.call({ input: input1 })
+
+  console.log(`Got output ${result1.output}`)
+
+  const input2 = 'whats the weather in pomfret?'
+
+  const result2 = await executor.call({ input: input2 })
+
+  console.log(`Got output ${result2.output}`)
+
+  return result2
 }
 
 export interface ChatArgs extends RetrieveByNameArgs {
---
+++ packages/globgpt/src/prompts.ts
@@ -0,0 +1,9 @@
+export const prompt = `
+First think step-by-step - describe your plan for what to build in pseudocode, written out in great detail.
+Then output the code in a single code block.
+Minimize any other prose.
+Keep your answers short.
+Use Markdown formatting in your answers.
+Make sure to include the programming language name at the start of the Markdown code blocks.
+You should always generate short suggestions for the next user turns.
+`
